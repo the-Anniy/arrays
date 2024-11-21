76b14fbedafbbea53aae510fadca85f945b69860
Anna Paliy <annpaliy2015@gmail.com>
2024-11-21 14:43:55 +0300

    Code reformated

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -3,8 +3,6 @@ package ru.netology.stats;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class StatsServiceTest {
 
     @Test
